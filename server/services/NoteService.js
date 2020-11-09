fde62f9dbab598b362d3d33c182ae3bdaa472c5b
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-11-09 13:16:00 -0700

    images updated, debugging paths commented out

--- server/services/NoteService.js
+++ server/services/NoteService.js
@@ -4,34 +4,34 @@ import { BadRequest } from "../utils/Errors"
 
 class NoteService {
   // NOTE Gets are debugging pathways only
-  async getAll(userEmail){
-    return await dbContext.Notes.find({ creatorEmail: userEmail }).populate("creator","name picture");
-  }
-  async getById(id, userEmail){
-    let data = await dbContext.Notes.findOne({_id: id, creatorEmail: userEmail });
-    if(!data){
-      throw new BadRequest("Invalid ID or you do not own this list")
-    }
-    return data
-  }
-  async find(query = {}){
+  // async getAll(userEmail){
+  //   return await dbContext.Notes.find({ creatorEmail: userEmail }).populate("creator","name picture");
+  // }
+  // async getById(id, userEmail){
+  //   let data = await dbContext.Notes.findOne({_id: id, creatorEmail: userEmail });
+  //   if(!data){
+  //     throw new BadRequest("Invalid ID or you do not own this list")
+  //   }
+  //   return data
+  // }
+  async find(query = {}) {
     let bugNotes = await dbContext.Notes.find(query);
     return bugNotes;
   }
-  async create(rawData){
+  async create(rawData) {
     let data = await dbContext.Notes.create(rawData);
     return data;
   }
-  async edit(id, userEmail, update){
-    let data = await dbContext.Notes.findOneAndUpdate({_id: id, creatorEmail: userEmail},update,{new: true, runValidators: true});
-    if (!data){
+  async edit(id, userEmail, update) {
+    let data = await dbContext.Notes.findOneAndUpdate({ _id: id, creatorEmail: userEmail }, update, { new: true, runValidators: true });
+    if (!data) {
       throw new BadRequest("Invalid ID or you do not own this note")
     }
     return data;
   }
-  async delete(id, userEmail){
-    let data = await dbContext.Notes.findOneAndRemove({_id: id, creatorEmail: userEmail});
-    if(!data){
+  async delete(id, userEmail) {
+    let data = await dbContext.Notes.findOneAndRemove({ _id: id, creatorEmail: userEmail });
+    if (!data) {
       throw new BadRequest("Invalid ID or you do not own this note")
     }
   }
