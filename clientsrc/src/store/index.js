9c084f23d30eb7e74a8ff01f653be54dcf35c5c3
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-11-10 17:06:35 -0700

    fixing a typo

--- clientsrc/src/store/index.js
+++ clientsrc/src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import router from "../router";
-import SweetALert from "../services/SweetAlert";
 import { api } from "./AxiosService"
 import SweetAlert from "../services/SweetAlert";
 
